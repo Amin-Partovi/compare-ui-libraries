626e8c3649a357cdb53146f4fe316d490f061827
Amin Partovi <amin.frontend@gmail.com>
2023-09-26 16:11:32 +0330

    feat: add shadcn components

---
+++ app/shadcn/page.js
@@ -0,0 +1,46 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Slider } from "@/components/ui/slider";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+
+const Shadcn = () => {
+  const array = [...Array(20).keys()];
+  return (
+    <>
+      <Input type="email" placeholder="Email" />
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a fruit" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Fruits</SelectLabel>
+            {array.map((item) => (
+              <SelectItem value="option1">Option {item}</SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Slider
+        defaultValue={[50]}
+        max={100}
+        step={1}
+        className={cn("w-[60%]", className)}
+        {...props}
+      />
+      <Button>Button</Button>
+    </>
+  );
+};
+
+export default Shadcn;
